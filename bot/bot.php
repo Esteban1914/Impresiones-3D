a96875e69f09d8a018e654258b63fa851bc2adda
Esteban <acevedoesteban999@gmail.com>
2024-01-01 16:01:29 -0500

    .

--- bot/bot.php
+++ bot/bot.php
@@ -30,17 +30,16 @@
                 {
                     $resp=$bot->registerUser($usernametelegram,$chatID,$username);
                     if($resp===true)
-                        $message="Usuario confirmado. Continue en proceso de registro en la plataforma";
+                        return "Usuario confirmado. Continue en proceso de registro en la plataforma";
                     else if ($resp===null)
-                        $message="No existe el usuario: ".$username." en la paltaforma";
+                        return "No existe el usuario: ".$username." en la paltaforma";
                     else    
-                        $message="Ha ocurrido un error en el proceso :(";
-                    return $message;
+                        return "Ha ocurrido un error en el proceso :(";
                 }
                 if(count($array)==2)
-                    regAndMess($bot,$usernametelegram,$chatID,$array[1]);   
+                    $message=regAndMess($bot,$usernametelegram,$chatID,$array[1]);   
                 else if(!empty($replay) && count(explode(" ",$reply['text']))===1)
-                    regAndMess($bot,$usernametelegram,$chatID,$reply_to_message);
+                    $message=regAndMess($bot,$usernametelegram,$chatID,$reply_to_message);
                 else 
                     $message="Escriba el comando /vincular seguido del nombre de usuario de la paltaforma, o haga referencia al mismo \nEjemplo: /vincular usuario";
             }
