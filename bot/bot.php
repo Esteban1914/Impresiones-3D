5e44d8031599aa961f53c447a7a42b9d7c8c1dc9
Esteban <acevedoesteban999@gmail.com>
2024-01-02 00:25:17 -0500

    .

--- bot/bot.php
+++ bot/bot.php
@@ -79,7 +79,6 @@
             {
                 $message="Ficheros STL\n";
                 $files=$bot->getFilesNamesByChatID($chatID);
-                $meessge=$message.json_encode($files);
                 foreach ($files as $row) {
                     $message = $message.$row['file_id']."\n";
                 }
