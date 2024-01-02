395dab23b925930543186ae27d83706540db0403
Esteban <acevedoesteban999@gmail.com>
2024-01-02 16:36:57 -0500

    .

--- profile.php
+++ profile.php
@@ -80,7 +80,7 @@
                     <div class="col-2">
                     </div>
                     <div class="col-8">
-                        <div class="card-title display-6 ">Editar Usuario</div>
+                        <div class="card-title display-6 ">Editar Contraseña    </div>
                     </div>
                     <div class="col-2">
                         <div class="d-none spinner-border" id="spiner_loaduser" role="status">
