6494c57571bba395d6406190c2e42990cc723a02
Esteban <acevedoesteban999@gmail.com>
2024-09-06 19:54:44 -0400

    [FIX] No validation

--- database/create.php
+++ database/create.php
@@ -1,51 +1,54 @@
 <?php
-    $servername="localhost";
-    $username="root";
-    $password= "";
-
+    include_once "../includes/session.php";
+    $s = new Session();
+    $host = getenv('DB_HOST');
+    $user = getenv('DB_USER');
+    $password = getenv('DB_PASS');
     ///////////////////////CONNECT///////////////////////
-    // try {
-    //     $conn = new PDO("mysql:host=$servername;", $username, $password);
-    //     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     echo "CONNECTED -> OK<br>";
-    // } catch(PDOException $e) {
-    //     die ("Error:" . $e->getMessage());
-    // }
+    try {
+        $conn = new PDO("mysql:host=$host;", $user, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "CONNECTED -> OK<br>";
+    } catch(PDOException $e) {
+        die ("Error:" . $e->getMessage());
+    }
     
     // // ///////////////////////CREATE DB///////////////////////
-    // try {
-    //     $sql = "CREATE DATABASE impresiones3D";
-    //     $conn->exec($sql);
-    //     echo "CREATED DB -> OK<br>";
-    // } catch(PDOException $e) {
-    //     die ("Error:" . $e->getMessage());
-    // }
+    try {
+        $sql = "CREATE DATABASE impresiones3D";
+        $conn->exec($sql);
+        echo "CREATED DB -> OK<br>";
+    } catch(PDOException $e) {
+        die ("Error:" . $e->getMessage());
+    }
     
 
     // // ///////////////////////CONNECT DB///////////////////////
-    // $conn->exec("USE impresiones3D");
-    // echo "SELECT -> OK<br>";
+    $conn->exec("USE impresiones3D");
+    echo "USE -> OK<br>";
     
-    // // ///////////////////////CREATE TABLE///////////////////////
+    ///////////////////////////CREATE TABLES///////////////////////
     
-    
-    "CREATE TABLE users (
+    $sql = "CREATE TABLE users (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(10) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         role ENUM('dev','admin','user') NOT NULL DEFAULT 'user',
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
+    $conn->exec($sql);
        
-    "CREATE TABLE user_validation (
+    $sql = "CREATE TABLE user_validation (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         type ENUM('email','phone') NOT NULL,
         data VARCHAR(254) NOT NULL UNIQUE,
         user_id INT(11) UNSIGNED NOT NULL UNIQUE,
         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )";
+    $conn->exec($sql);
+    
 
-    "CREATE TABLE user_telegram (
+    $sql = "CREATE TABLE user_telegram (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(20) NOT NULL,
         chat_id INT(11) NOT NULL,
@@ -54,8 +57,9 @@
         user_id INT(11) UNSIGNED NOT NULL UNIQUE,
         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )"; 
-
-    "CREATE TABLE files (
+    $conn->exec($sql);
+    
+    $sql = "CREATE TABLE files (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         file_id CHAR(100) UNIQUE NOT NULL,
         file_name CHAR(20) NOT NULL,
@@ -63,29 +67,17 @@
         user_id INT(11) UNSIGNED NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )";
-           
-    "CREATE TABLE file_requests (
-        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        message TEXT,
-        state ENUM('ACCEPT','DENIED','COMPLETED'),
-        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        file_id INT(11) UNSIGNED UNIQUE,
-        FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE,
-        user_admin INT(11) UNSIGNED,
-        FOREIGN KEY (user_admin) REFERENCES users(id) ON DELETE CASCADE
-        filament_id INT(11) UNSIGNED NOT NULL,
-        FOREIGN KEY (filament_id) REFERENCES filament(id) ON DELETE CASCADE
-        filament_color_id INT(11) UNSIGNED NOT NULL,
-        FOREIGN KEY (filament_color_id) REFERENCES filament_color(id) ON DELETE CASCADE
-    )";
+    $conn->exec($sql);
     
-    "CREATE TABLE filament (
+    $sql = "CREATE TABLE filament (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(30) NOT NULL UNIQUE,
         active BOOLEAN DEFAULT FALSE,
         price DECIMAL(8,3) 
     )";
-    "CREATE TABLE filament_color (
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE filament_color (
         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(20) NOT NULL,
         -- R DECIMAL(3,3) NOT NULL,
@@ -93,23 +85,45 @@
         -- B DECIMAL(3,3) NOT NULL 
         color VARCHAR(8) DEFAULT '#000000'
     )";
-    "CREATE TABLE filament_color_relation (
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE filament_color_relation (
         filament_id INT(11) UNSIGNED,
         color_id INT(11) UNSIGNED,
         active BOOLEAN DEFAULT FALSE,
         PRIMARY KEY (filament_id, color_id),
         FOREIGN KEY (filament_id) REFERENCES filament(id) ON DELETE CASCADE,
         FOREIGN KEY (color_id) REFERENCES filament_color(id) ON DELETE CASCADE
-     );"
-            
+     );";
+    $conn->exec($sql);
 
+    $sql = "CREATE TABLE file_requests (
+        id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        message TEXT,
+        state ENUM('ACCEPT', 'DENIED', 'COMPLETED'),
+        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        file_id INT(11) UNSIGNED UNIQUE,
+        user_admin INT(11) UNSIGNED,
+        filament_id INT(11) UNSIGNED NOT NULL,
+        filament_color_id INT(11) UNSIGNED NOT NULL,
+        FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE,
+        FOREIGN KEY (user_admin) REFERENCES users(id) ON DELETE CASCADE,
+        FOREIGN KEY (filament_id) REFERENCES filament(id) ON DELETE CASCADE,
+        FOREIGN KEY (filament_color_id) REFERENCES filament_color(id) ON DELETE CASCADE
+    )";
+    $conn->exec($sql);
+
+    
+
+    echo "TABLES CRATED -> OK<br>";
+    
     // // ///////////////////////INSERT TABLE///////////////////////
-    // $sql="INSERT INTO users (username,password) VALUES (:u,:p)";
-    // $query=$conn->prepare($sql);
-    // if($query->execute([":u"=> "ernesto","p"=> "12345678"]))
-    //     echo "CREATED ESTEBAN -> OK<br>";
-    // else
-    //     echo "NO CREATED ESTEBAN -> X<br>";
+    $sql="INSERT INTO users (username,password) VALUES (:u,:p)";
+    $query=$conn->prepare($sql);
+    if($query->execute([":u"=> "Esteban","p"=> "1234567890"]))
+        echo "CREATED ESTEBAN -> OK<br>";
+    else
+        echo "NO CREATED ESTEBAN -> X<br>";
     
     // $sql="INSERT INTO user_telegram (username,user_id) VALUES (:un,:ui)";
     // $query=$conn->prepare($sql);
@@ -178,8 +192,3 @@
     // }
     // else
     //     echo "NO ALLTER -> X<br>";
-
-
-
-    
-?>
--- includes/register.php
+++ includes/register.php
@@ -6,44 +6,44 @@
             
             include_once "bot.php";
             $bot=new Bot();
-            if($_POST['validation_type']=="email")
-            {    
-                $apiKey = "cd0f40e95d6843abb188225988c17314"; 
-                $response = file_get_contents("https://emailvalidation.abstractapi.com/v1?api_key=$apiKey&email=".$_POST['validation_data']);
-                if($response!==false)
-                {    
-                    $data = json_decode($response, true);
-                    if (!$data['is_smtp_valid']['value']) 
-                    {
-                        header ("Location: ../signin.php?signin=BAD_VALIDATION&method=".$_POST['validation_type']."&data=".$_POST['validation_data']);
-                        exit();
-                    }
-                }
-                else
-                {
-                    header ("Location: ../signin.php?signin=BAD");
-                    exit();
-                }
-            }
-            else
-            {
-                $apiKey = "96286f6055f84305b74a85b6c264c082"; 
-                $response = file_get_contents("https://phonevalidation.abstractapi.com/v1/?api_key=$apiKey&phone=".$_POST['validation_data']);
-                if($response!==false)
-                {
-                    $data = json_decode($response, true);
-                    if (!$data['valid']) 
-                    {
-                        header ("Location: ../signin.php?signin=BAD_VALIDATION&method=".$_POST['validation_type']."&data=".$_POST['validation_data']);
-                        exit();
-                    }
-                }
-                else
-                {
-                    header ("Location: ../signin.php?signin=BAD");
-                    exit();
-                }
-            }
+            // if($_POST['validation_type']=="email")
+            // {    
+            //     $apiKey = "cd0f40e95d6843abb188225988c17314"; 
+            //     $response = file_get_contents("https://emailvalidation.abstractapi.com/v1?api_key=$apiKey&email=".$_POST['validation_data']);
+            //     if($response!==false)
+            //     {    
+            //         $data = json_decode($response, true);
+            //         if (!$data['is_smtp_valid']['value']) 
+            //         {
+            //             header ("Location: ../signin.php?signin=BAD_VALIDATION&method=".$_POST['validation_type']."&data=".$_POST['validation_data']);
+            //             exit();
+            //         }
+            //     }
+            //     else
+            //     {
+            //         header ("Location: ../signin.php?signin=BAD");
+            //         exit();
+            //     }
+            // }
+            // else
+            // {
+            //     $apiKey = "96286f6055f84305b74a85b6c264c082"; 
+            //     $response = file_get_contents("https://phonevalidation.abstractapi.com/v1/?api_key=$apiKey&phone=".$_POST['validation_data']);
+            //     if($response!==false)
+            //     {
+            //         $data = json_decode($response, true);
+            //         if (!$data['valid']) 
+            //         {
+            //             header ("Location: ../signin.php?signin=BAD_VALIDATION&method=".$_POST['validation_type']."&data=".$_POST['validation_data']);
+            //             exit();
+            //         }
+            //     }
+            //     else
+            //     {
+            //         header ("Location: ../signin.php?signin=BAD");
+            //         exit();
+            //     }
+            // }
             
             if($bot->registerUser($_POST["username"],$_POST["password"],$_POST['validation_type'],$_POST['validation_data']))
             {
